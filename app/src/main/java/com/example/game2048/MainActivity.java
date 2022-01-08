f7c42ffd998a4a14cae755b8a56beddff61644bd
mayorapril <mayorapril@thienv460@gmail.com>
2022-01-08 07:28:36 +0700

    Push noti

--- app/src/main/java/com/example/game2048/MainActivity.java
+++ app/src/main/java/com/example/game2048/MainActivity.java
@@ -17,30 +17,29 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button playgame, choitiep;
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            try{
+            try {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage("Quit?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
                         Intent startMain = new Intent(Intent.ACTION_MAIN);
-
                         startMain.addCategory(Intent.CATEGORY_HOME);
                         startActivity(startMain);
                         sendNotification();
                         finish();
-                        System.exit(0);
+//                        System.exit(0);
                     }
                 })
-                               .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                                   public void onClick(DialogInterface dialog, int id) {
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
 
-                                   }
-                               }).show();
-            }catch (Exception e)
-            {
+                            }
+                        }).show();
+            } catch (Exception e) {
                 return true;
             }
         }
@@ -59,9 +58,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public void batxukienclick()
-    {
+    public void batxukienclick() {
         playgame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     private void sendNotification() {
         Toast.makeText(MainActivity.this, "Notification Set!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(MainActivity.this, NotifyBroadcast.class);
-        PendingIntent pendingIntent  = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
         long timeAtButtonClick = System.currentTimeMillis();
@@ -108,18 +105,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void anhxa()
-    {
-        playgame = (Button)findViewById(R.id.playgame2048);
-        choitiep = (Button)findViewById(R.id.choitiep);
+    public void anhxa() {
+        playgame = (Button) findViewById(R.id.playgame2048);
+        choitiep = (Button) findViewById(R.id.choitiep);
     }
 
-<<<<<<< HEAD
-
-=======
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
->>>>>>> d6fcee0de260582be1816d0e58c24cef83a4fb14
 }
